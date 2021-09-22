877a1fd3b7697cb42c86699c0274216f1a260e0a
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-09-21 17:34:35 -0700

    passing test

--- __tests__/static-server.test.js
+++ __tests__/static-server.test.js
@@ -1,6 +1,6 @@
-import request from 'supertest';
-import app from '../lib/app.js';
-import { readFile } from 'fs/promises';
+const request = require('supertest');
+const app = require('../lib/app.js');
+const { readFile } = require('fs/promises');
 
 describe('Routes for static server', () => {
   it('should return index.html', async () => {
@@ -10,4 +10,10 @@ describe('Routes for static server', () => {
     ]);  
     expect(response.text).toEqual(index);
   });
+  // it('it tests for the return of CSS from GET', async() => {
+  //   const [response, file] = await Promise.all([
+  //     request(app).get('/styles/main.css'),
+  //     readFile('./public/styles/main.css', 'utf-8')
+  //   ]);
+  // });
 });
